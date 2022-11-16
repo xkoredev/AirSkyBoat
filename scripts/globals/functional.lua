c9c0ee0f00e2e181981e835a7bc03ced40de78cd
Kore <d.fernsboi@gmail.com>
2022-11-16 12:03:37 -0800

    Finish up entry requirements

---
+++ scripts/globals/functional.lua
@@ -0,0 +1,87 @@
+-----------------------------------
+--
+-- Functional
+--
+-- Functional methods provide a means to simplify logic that consists in
+-- simple operations when iterating a table.
+-- In general, they can make code much more concise and readable, but they
+-- can also end up making it a cluttered mess, so use your judgement
+-- when deciding if you want to use these methods
+-----------------------------------
+
+fn = {}
+
+-- Given a table and a filter function, returns a new table composed of the
+-- elements that pass the given filter.
+-- e.g;
+-- fn.filter({"a", "b", "c", "d"}, function(k, v) return v >= "c" end)  --> {"c","d"}
+function fn.filter(tbl, func)
+    local out = {}
+
+    for k, v in pairs(tbl) do
+        if func(k, v) then
+            out[k] = v
+        end
+    end
+
+    return out
+end
+
+-- Given a table and a filter function, returns a new table composed of the
+-- elements that pass the given filter.
+-- Unlike fn.filter, this method will return an iterable table.
+-- e.g;
+-- fn.filter({"a", "b", "c", "d"}, function(v) return v >= "c" end)  --> {1 => "c", 2 => "d"}
+function fn.filterArray(tbl, func)
+    local out = {}
+
+    for k, v in pairs(tbl) do
+        if func(k, v) then
+            table.insert(out, v)
+        end
+    end
+
+    return out
+end
+
+-- Returns true if any member of the given table passes the given
+-- predicate function
+function fn.any(tbl, predicate)
+    for k, v in pairs(tbl) do
+        if predicate(k, v) then
+            return true
+        end
+    end
+
+    return false
+end
+
+-- Returns the sum of applying the given function to each element of the given table
+-- fn.sum({1, 2, 3}, function(k, v) return v end)  --> 6
+function fn.sum(tbl, func)
+    local sum = 0
+
+    for k, v in pairs(tbl) do
+        print(k)
+        print(v)
+        sum = sum + func(k, v)
+    end
+
+    return sum
+end
+
+-- To be used with fn.sum.
+-- Used to count the number of times an element in a table
+-- matches the given predicate
+-- e.g: fn.sum({"a, "a", "b"}, fn.counter(function (k,v) return v == "a" end)) --> 2
+function fn.counter(predicate)
+    return function (k, v)
+        print(k)
+        print(v)
+        if predicate(k, v) then
+            return 1
+        else
+            return 0
+        end
+    end
+end
--- scripts/globals/garrison.lua
+++ scripts/globals/garrison.lua
@@ -1,19 +1,19 @@
 -----------------------------------
 -- Garrison
 -----------------------------------
-require('scripts/globals/mobs')
 require('scripts/globals/common')
+require('scripts/globals/functional')
+require('scripts/globals/garrison_data')
 require('scripts/globals/items')
+require('scripts/globals/mobs')
 require('scripts/globals/npc_util')
+require('scripts/globals/pathfind')
 require('scripts/globals/status')
 require('scripts/globals/utils')
 require('scripts/globals/zone')
-require('scripts/globals/pathfind')
-require('scripts/globals/garrison_data')
 -----------------------------------
 xi = xi or {}
 xi.garrison = xi.garrison or {}
-xi.garrison.lookup = xi.garrison.lookup or {}
 
 xi.garrison.state =
 {
@@ -27,7 +27,7 @@ xi.garrison.state =
 }
 
 -----------------------------------
--- Helpers: Debug
+-- Helpers: Logging / Messaging
 -----------------------------------
 
 -- Prints the given message if DEBUG_GARRISON is enabled
@@ -56,6 +56,12 @@ local debugPrintToPlayers = function(zoneData, msg)
     end
 end
 
+local messagePlayers = function(npc, players, msg)
+    for _,player in ipairs(players) do
+        player:messageText(npc, msg)
+    end
+end
+
 -----------------------------------
 -- Helpers: Spawning
 -----------------------------------
@@ -71,11 +77,13 @@ xi.garrison.addLevelCap = function(entity, cap)
         xi.effect.LEVEL_RESTRICTION,
         cap,
         0,
+        xi.settings.main.GARRISON_TIME_LIMIT,
         0,
         0,
         0,
-        0,
-        xi.effectFlag.DEATH + xi.effectFlag.ON_ZONE + xi.effectFlag.CONFRONTATION)
+        -- Note the level restriction does not wear on death.
+        -- Some sources say it does, but we have captures showing otherwise.
+        xi.effectFlag.ON_ZONE + xi.effectFlag.CONFRONTATION)
 end
 
 -- Randomly assigns aggro between the given groups of entity IDs
@@ -290,7 +298,9 @@ xi.garrison.tick = function(npc)
             -- case 1: Either npcs or players are dead. End event.
             local allNPCsDead = #zoneData.npcs == zoneData.deadNPCCount
             if allNPCsDead or allPlayersDead then
-                debugPrintToPlayers(zoneData, "Mission failed")
+                -- You fought hard, and you proved yourself worthy...
+                debugPrintToPlayers(zoneData, "Mission failed by death")
+                messagePlayers(npc, zoneData.players, ID.text.GARRISON_BASE + 19)
                 zoneData.state = xi.garrison.state.ENDED
                 return
             end
@@ -313,11 +323,24 @@ xi.garrison.tick = function(npc)
             -- case 4: All Mobs despawned and this was last group. Check if we advance to next wave
             if allMobsDespawned and isLastGroup and not isLastWave then
                 zoneData.state = xi.garrison.state.ADVANCE_WAVE
+                return
             end
 
             -- case 5: All mobs are dead and this was last group and last wave. Grant loot.
             if allMobsDead and isLastGroup and isLastWave and zoneData.bossSpawned then
                 zoneData.state = xi.garrison.state.GRANT_LOOT
+                return
+            end
+
+            -- case 6: Timeout
+            -- TODO: Players need to get entity
+            local lvlCapCounter = fn.counter(function(_,v) return v:hasStatusEffect(xi.effect.LEVEL_RESTRICTION) end)
+            if fn.sum(zoneData.players, lvlCapCounter) == 0 then
+                -- You fought hard, and you proved yourself worthy...
+                debugPrintToPlayers(zoneData, "Mission failed by timeout")
+                messagePlayers(npc, zoneData.players, ID.text.GARRISON_BASE + 19)
+
+                zoneData.state = xi.garrison.state.ENDED
             end
         end,
 
@@ -384,6 +407,7 @@ xi.garrison.tick = function(npc)
             debugLog("State: Grant Loot")
             debugPrintToPlayers(zoneData, "Mission success")
 
+            messagePlayers(npc, zoneData.players, ID.text.GARRISON_BASE + 39)
             xi.garrison.handleLootRolls(xi.garrison.loot[zoneData.levelCap], zoneData.players)
             zoneData.state = xi.garrison.state.ENDED
         end,
@@ -538,44 +562,65 @@ end
 -- * Player does not have more than GARRISON_PARTY_LIMIT alliance memebrs
 -- * Player is at least at GARRISON_RANK rank level
 xi.garrison.validateEntry = function(zoneData, player, npc, guardNation)
+    local ID = zones[player:getZoneID()]
     if zoneData.continue then
         debugLog("Another garrison in progress")
         player:messageText(npc, ID.text.GARRISON_BASE + 1)
         return false
     end
 
+    local sameZone = function (_,v) return v ~= nil and v:getZoneID() == player:getZoneID() end
+    local membersInZone = fn.filterArray(player:getAlliance(), sameZone)
+
     -- This assumes that only the player trading the item has to be from the right nation
-    local ID = zones[player:getZoneID()]
     if xi.settings.main.GARRISON_NATION_BYPASS == false and guardNation ~= player:getNation() then
         debugLog("Outpost not controller by player's nation")
-        print(zoneData.itemReq)
         player:messageSpecial(ID.text.GARRISON_BASE + player:getNation(), zoneData.itemReq)
         return false
     end
 
-    if utils.any(zoneData.players, function(_, v) return v:isLevelSync() end) then
-        debugLog("Your party is unable to participate because certain members' levels are restricted")
+    if fn.any(membersInZone, function(_, v) return v:isLevelSync() end) then
+        -- Your party is unable to participate because certain members' levels are restricted
+        debugLog("One or more alliance members have level sync on")
         player:messageText(npc, ID.text.MEMBERS_LEVELS_ARE_RESTRICTED, false)
         return false
     end
 
-    if utils.sum(zoneData.players, function(k, v) return 1 end) > xi.settings.main.GARRISON_PARTY_LIMIT then
+    if #membersInZone > xi.settings.main.GARRISON_PARTY_LIMIT then
+        -- This is a custom message. I don't believe retail has this limitation
         debugLogf("Alliance exceeds member limit: %d", xi.settings.main.GARRISON_PARTY_LIMIT)
+        player:PrintToPlayer(printf("Maximum garrison alliance size is %d", xi.settings.main.GARRISON_PARTY_LIMIT))
         return false;
     end
 
-    local meetsRank = function(_, v) return v:getRank(v:getNation()) >= xi.settings.main.GARRISON_RANK end
-    if utils.any(zoneData.players, not meetsRank) then
+    local doesNotMeetRank = function(_, v)
+        return v:getRank(v:getNation()) < xi.settings.main.GARRISON_RANK
+    end
+    if fn.any(membersInZone, doesNotMeetRank) then
+        -- These young participants are quite spirited, but they lack valuable battle experience
         debugLogf("Leader does not meet required rank: %d", xi.settings.main.GARRISON_RANK)
+        player:messageText(npc, ID.text.GARRISON_BASE + 4)
         return false;
     end
 
-    if xi.garrison.isAnyPlayerOnEntryCooldown(zoneData.players) then
+    if xi.garrison.isAnyPlayerOnEntryCooldown(membersInZone) then
+        -- We commend you on your services in helping us avert the beastmen's attack. I do not see them attacking us again any time soon
         debugLogf("Alliance members on cooldown")
+        player:messageText(npc, ID.text.GARRISON_BASE + 40)
         return false;
     end
 
     return true
+end
+
+-- Stores data related to player entry time / lockout
+xi.garrison.registerPlayerEntry = function(player)
+    local nextEntryTime = os.time() + xi.settings.main.GARRISON_LOCKOUT
+    if xi.settings.main.GARRISON_ONCE_PER_WEEK then
+        nextEntryTime = math.max(nextEntryTime, getConquestTally())
+    end
+
+    player:setCharVar("[Garrison]NextEntryTime", nextEntryTime)
 end
 
 -- Returns true if any player in the given table has entered garrison too recently
@@ -590,14 +635,4 @@ xi.garrison.isAnyPlayerOnEntryCooldown = function(players)
     end
 
     return false
-end
-
--- Stores data related to player entry time / lockout
-xi.garrison.registerPlayerEntry = function(player)
-    local nextEntryTime = os.time() + xi.main.GARRISON_LOCKOUT
-    if xi.main.GARRISON_ONCE_PER_WEEK then
-        nextEntryTime = math.max(nextEntryTime, getConquestTally())
-    end
-
-    player:setCharVar("[Garrison]NextEntryTime", nextEntryTime)
 end
